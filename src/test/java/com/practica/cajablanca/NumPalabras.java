06d7adbf00cbce5b2e3d8fad9d90b2446b976055
Santiago <S.tmarco@alumnos.upm.es>
2021-11-29 09:53:18 +0100

    ficheros

---
+++ src/test/java/com/practica/cajablanca/NumPalabras.java
@@ -0,0 +1,25 @@
+package com.practica.cajablanca;
+
+
+import com.cajanegra.AbstractSingleLinkedListImpl;
+import com.cajanegra.EmptyCollectionException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumPalabras {
+
+    private Editor miEditor;
+    private AbstractSingleLinkedListImpl<String> miLista;
+
+    
+}
